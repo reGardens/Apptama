1215c858312b26a76839e386c31543fbb211331d
rezabaguspratama <reza.pratama@pcsindonesia.co.id>
2025-11-27 17:36:24 +0700

    fix: adj title

--- src/pages/note/Note.tsx
+++ src/pages/note/Note.tsx
@@ -2,7 +2,7 @@ const Note = () => {
     return (
         <>
             <h1>Aplikasi Keluarga Pratama</h1>
-            <div>Aplikasi berisi feature login, catatan, dan pengeluaran, dst</div>
+            <div>Aplikasi berisi feature login, catatan, dan pengeluaran,</div>
         </>
     )
 }
